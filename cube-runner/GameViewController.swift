44aa63e575c3ea864cb7cf529a83ee45c227c4ab
liusy182 <liusy182@gmail.com>
2016-04-30 16:26:28 +0800

    added 3d model.

--- cube-runner/GameViewController.swift
+++ cube-runner/GameViewController.swift
@@ -5,121 +5,59 @@
 //  Created by liusy182 on 24/4/16.
 //  Copyright (c) 2016 liusy182. All rights reserved.
 //
-
 import UIKit
 import QuartzCore
 import SceneKit
 
 class GameViewController: UIViewController {
-
+    private let scnView = SCNView()
+    private var scene: SCNScene!
+    private var cameraNode: SCNNode!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        scnView.frame = view.bounds
+        view.addSubview(scnView)
         
-        // create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // create and add a camera to the scene
-        let cameraNode = SCNNode()
-        cameraNode.camera = SCNCamera()
-        scene.rootNode.addChildNode(cameraNode)
-        
-        // place the camera
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
-        
-        // create and add a light to the scene
-        let lightNode = SCNNode()
-        lightNode.light = SCNLight()
-        lightNode.light!.type = SCNLightTypeOmni
-        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
-        scene.rootNode.addChildNode(lightNode)
-        
-        // create and add an ambient light to the scene
-        let ambientLightNode = SCNNode()
-        ambientLightNode.light = SCNLight()
-        ambientLightNode.light!.type = SCNLightTypeAmbient
-        ambientLightNode.light!.color = UIColor.darkGrayColor()
-        scene.rootNode.addChildNode(ambientLightNode)
-        
-        // retrieve the ship node
-        let ship = scene.rootNode.childNodeWithName("ship", recursively: true)!
-        
-        // animate the 3d object
-        ship.runAction(SCNAction.repeatActionForever(SCNAction.rotateByX(0, y: 2, z: 0, duration: 1)))
-        
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
-        
-        // set the scene to the view
-        scnView.scene = scene
-        
-        // allows the user to manipulate the camera
-        scnView.allowsCameraControl = true
-        
-        // show statistics such as fps and timing information
-        scnView.showsStatistics = true
-        
-        // configure the view
-        scnView.backgroundColor = UIColor.blackColor()
-        
-        // add a tap gesture recognizer
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(GameViewController.handleTap(_:)))
-        scnView.addGestureRecognizer(tapGesture)
+        createContents()
     }
-    
-    func handleTap(gestureRecognize: UIGestureRecognizer) {
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
-        
-        // check what nodes are tapped
-        let p = gestureRecognize.locationInView(scnView)
-        let hitResults = scnView.hitTest(p, options: nil)
-        // check that we clicked on at least one object
-        if hitResults.count > 0 {
-            // retrieved the first clicked object
-            let result: AnyObject! = hitResults[0]
-            
-            // get its material
-            let material = result.node!.geometry!.firstMaterial!
-            
-            // highlight it
-            SCNTransaction.begin()
-            SCNTransaction.setAnimationDuration(0.5)
-            
-            // on completion - unhighlight
-            SCNTransaction.setCompletionBlock {
-                SCNTransaction.begin()
-                SCNTransaction.setAnimationDuration(0.5)
-                
-                material.emission.contents = UIColor.blackColor()
-                
-                SCNTransaction.commit()
-            }
-            
-            material.emission.contents = UIColor.redColor()
-            
-            SCNTransaction.commit()
-        }
-    }
-    
-    override func shouldAutorotate() -> Bool {
-        return true
-    }
-    
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
-    
-    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
-        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-            return .AllButUpsideDown
-        } else {
-            return .All
-        }
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
 }
+
+// MARK: content builder
+private extension GameViewController {
+    func createContents() {
+        scene = SCNScene(named: "art.scnassets/eurofighter.dae")
+        scnView.showsStatistics = true
+        scnView.debugOptions = .ShowPhysicsShapes
+        
+        let jetfighterNode = createJetfighter()
+        
+        cameraNode = createCamera()
+        scene.rootNode.addChildNode(cameraNode)
+        
+        scnView.scene = scene
+    }
+    
+    func createCamera() -> SCNNode{
+        let cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        cameraNode.position = SCNVector3Make(0, 7, 20)
+        cameraNode.rotation = SCNVector4Make(1, 0, 0, -atan2f(7, 20.0))
+        return cameraNode
+    }
+    
+    func createJetfighter() -> SCNNode{
+        let jetfighterNode = scene!.rootNode.childNodeWithName("jetfighter", recursively: true)!
+        
+        jetfighterNode.scale = SCNVector3(x: 0.03, y: 0.03, z: 0.03)
+        jetfighterNode.position = SCNVector3(x: 0, y: 1.0, z: 13)
+        jetfighterNode.rotation = SCNVector4(x: 0, y: 1, z: 0, w: Float(M_PI))
+        return jetfighterNode
+    }
+    
+}
+
+
